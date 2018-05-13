362ec26d499606f933fd6c25cf9e718b7a18be03
Daniel Drywa <daniel@drywa.me>
2018-05-13 19:21:33 +0100

    Adding API docs for Options.Option.

--- GetOpts/Options.cs
+++ GetOpts/Options.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
 
 namespace DD.GetOpts {
     /// <summary>
@@ -37,7 +39,7 @@ namespace DD.GetOpts {
     }
 
     /// <summary>
-    /// The occurence of a <see cref="Option" /> argument.
+    /// The occurence of a argument after the <see cref="Option" />.
     /// </summary>
     /// <remarks>
     /// <para>Added in version 0.1.0.</para>
@@ -66,6 +68,368 @@ namespace DD.GetOpts {
         /// <para>Added in version 0.1.0.</para>
         /// </remarks>
         OPTIONAL = 0x02,
+    }
+
+    public sealed class Options {
+        private const string SHORT_PREFIX = "-";
+        private const string LONG_PREFIX = "--";
+
+        private readonly HashSet<Option> required = new HashSet<Option>();
+        private readonly HashSet<Option> allOptions = new HashSet<Option>();
+        private readonly Dictionary<string, Option> shortOptions = new Dictionary<string, Option>();
+        private readonly Dictionary<string, Option> longOptions = new Dictionary<string, Option>();
+
+        public Options Add( string shortName, string longName, Argument arguments, Occur occurs ) {
+            if ( shortName == null ) {
+                throw new ArgumentNullException( nameof( shortName ) );
+            }
+            if ( longName == null ) {
+                throw new ArgumentNullException( nameof( longName ) );
+            }
+            if ( ( byte )arguments > 0x02 ) {
+                throw new ArgumentException( $"Invalid {nameof( arguments )} value {arguments}.", nameof( arguments ) );
+            }
+            if ( ( byte )occurs > 0x02 ) {
+                throw new ArgumentException( $"Invalid {nameof( occurs )} value {occurs}.", nameof( occurs ) );
+            }
+
+            shortName = shortName.Trim();
+            longName = longName.Trim();
+
+            if ( shortName == string.Empty && longName == string.Empty ) {
+                throw new ArgumentException( "Option must contain at least one valid name." );
+            }
+            if ( shortName.Any( x => char.IsWhiteSpace( x ) || char.IsControl( x ) ) ) {
+                throw new ArgumentException( "Short name must not contain control or white space characters.", nameof( shortName ) );
+            }
+            if ( longName.Any( x => char.IsWhiteSpace( x ) || char.IsControl( x ) ) ) {
+                throw new ArgumentException( "Long name must not contain control or white space characters.", nameof( longName ) );
+            }
+            if ( shortName.StartsWith( SHORT_PREFIX ) ) {
+                throw new ArgumentException( $"Short name starts with the short prefix {SHORT_PREFIX}.", nameof( shortName ) );
+            }
+            if ( shortName.StartsWith( LONG_PREFIX ) ) {
+                throw new ArgumentException( $"Short name starts with the long prefix {LONG_PREFIX}.", nameof( longName ) );
+            }
+            if ( longName.StartsWith( SHORT_PREFIX ) ) {
+                throw new ArgumentException( $"Long name starts with the short prefix {SHORT_PREFIX}.", nameof( longName ) );
+            }
+            if ( longName.StartsWith( LONG_PREFIX ) ) {
+                throw new ArgumentException( $"Long name starts with the long prefix {LONG_PREFIX}.", nameof( longName ) );
+            }
+
+            var option = new Option( shortName, longName, arguments, occurs );
+
+            if ( shortName != string.Empty ) {
+                try {
+                    shortOptions.Add( option.ShortName, option );
+                } catch ( ArgumentException ex ) {
+                    throw new ArgumentException( $"Option with short name {option.ShortName} already exists.", ex );
+                }
+            }
+
+            if ( longName != string.Empty ) {
+                try {
+                    longOptions.Add( option.LongName, option );
+                } catch ( ArgumentException ex ) {
+                    throw new ArgumentException( $"Option with long name {option.LongName} already exists.", ex );
+                }
+            }
+
+            allOptions.Add( option );
+            if ( option.Occurs == Occur.ONCE ) {
+                required.Add( option );
+            }
+            return this;
+        }
+
+        public bool ContainsShort( string shortName )
+            => shortOptions.ContainsKey( shortName );
+
+        public bool ContainsLong( string longName )
+            => longOptions.ContainsKey( longName );
+
+        public Matches Parse( IEnumerable<string> arguments ) {
+            var context = new Context( arguments.GetEnumerator() );
+
+            Read( ref context, null );
+
+            var shortMatches = new Dictionary<string, Match>();
+            var longMatches = new Dictionary<string, Match>();
+
+            var req = new HashSet<Option>( required );
+            foreach ( var option in context.Options ) {
+                var match = new Match(
+                    option.ShortName,
+                    option.LongName,
+                    context.Count[ option ],
+                    context.Arguments[ option ].AsReadOnly() );
+
+                if ( match.ShortName != string.Empty ) {
+                    shortMatches.Add( match.ShortName, match );
+                }
+
+                if ( match.LongName != string.Empty ) {
+                    longMatches.Add( match.LongName, match );
+                }
+
+                req.Remove( option );
+            }
+
+            if ( req.Count > 0 ) {
+                var arg = req.First();
+
+                throw new ArgumentException(
+                    CreateErrorMessage( "Expected argument ", arg.ShortName, arg.LongName ) );
+            }
+
+            return new Matches(
+                context.Free.AsReadOnly(),
+                new ReadOnlyDictionary<string, Match>( shortMatches ),
+                new ReadOnlyDictionary<string, Match>( longMatches ) );
+        }
+
+        private void Read( ref Context context, Option previous ) {
+            if ( !context.Enumerator.MoveNext() ) {
+                if ( previous?.Arguments == Argument.REQUIRED ) {
+                    throw new ArgumentException(
+                        CreateErrorMessage( "Expected argument after ", previous.ShortName, previous.LongName ) );
+                }
+                return;
+            }
+
+            var argument = context.Enumerator.Current.Trim();
+            var isShort = argument.StartsWith( SHORT_PREFIX );
+            var isLong = argument.StartsWith( LONG_PREFIX );
+
+            Option option = null;
+
+            if ( isLong && isShort ) {
+                if ( LONG_PREFIX.Length > SHORT_PREFIX.Length ) {
+                    option = ReadOption( ref context, previous, argument, LONG_PREFIX, longOptions );
+                } else {
+                    option = ReadOption( ref context, previous, argument, SHORT_PREFIX, shortOptions );
+                }
+
+                previous = option ?? throw new ArgumentException( $"Invalid argument {argument}." );
+
+            } else if ( isShort ) {
+                previous = ReadOption( ref context, previous, argument, SHORT_PREFIX, shortOptions )
+                    ?? throw new ArgumentException( $"Invalid argument {argument}." );
+
+            } else if ( isLong ) {
+                previous = ReadOption( ref context, previous, argument, LONG_PREFIX, longOptions )
+                    ?? throw new ArgumentException( $"Invalid argument {argument}." );
+
+            } else {
+                if ( previous?.Arguments != Argument.NONE ) {
+                    context.Arguments[ previous ].Add( argument );
+                } else {
+                    context.Free.Add( argument );
+                }
+
+                previous = null;
+            }
+
+            Read( ref context, previous );
+        }
+
+        private Option ReadOption(
+            ref Context context,
+            Option previous,
+            string argument,
+            string prefix,
+            Dictionary<string, Option> options ) {
+
+            if ( previous?.Arguments == Argument.REQUIRED ) {
+                throw new ArgumentException( $"Expected argument after {argument}." );
+            }
+
+            var name = argument.Remove( 0, prefix.Length );
+            if ( !options.TryGetValue( name, out var option ) ) {
+                return null;
+            }
+
+            context.Options.Add( option );
+            UpdateOptionOccurance( ref context, option );
+
+            if ( !context.Arguments.ContainsKey( option ) ) {
+                context.Arguments.Add( option, new List<string>() );
+            }
+            return option;
+        }
+
+        private void UpdateOptionOccurance( ref Context context, Option option ) {
+            if ( !context.Count.TryGetValue( option, out var count ) ) {
+                context.Count.Add( option, 1 );
+                return;
+            }
+
+            count += 1;
+            if ( option.Occurs != Occur.MULTIPLE && count > 1 ) {
+                throw new ArgumentException(
+                    CreateErrorMessage( "Multiple occurance of ", option.ShortName, option.LongName ) );
+            }
+
+            context.Count[ option ] = count;
+        }
+
+        private string CreateErrorMessage( string message, string shortName, string longName ) {
+            var sb = new StringBuilder( message );
+
+            if ( shortName != string.Empty && longName != string.Empty ) {
+                sb.Append( shortName )
+                    .Append( " or " )
+                    .Append( longName );
+
+            } else if ( shortName != string.Empty ) {
+                sb.Append( shortName );
+
+            } else if ( longName != string.Empty ) {
+                sb.Append( longName );
+            }
+
+            return sb.Append( '.' ).ToString();
+        }
+
+        private ref struct Context {
+            public List<string> Free {
+                get;
+            }
+
+            public HashSet<Option> Options {
+                get;
+            }
+
+            public Dictionary<Option, List<string>> Arguments {
+                get;
+            }
+
+            public Dictionary<Option, int> Count {
+                get;
+            }
+
+            public IEnumerator<string> Enumerator {
+                get;
+            }
+
+            public Context( IEnumerator<string> enumerator )
+                => (Enumerator, Free, Options, Arguments, Count)
+                = (enumerator,
+                new List<string>(),
+                new HashSet<Option>(),
+                new Dictionary<Option, List<string>>(),
+                new Dictionary<Option, int>());
+        }
+
+        /// <summary>
+        /// The command line option.
+        /// </summary>
+        private sealed class Option : IEquatable<Option> {
+            /// <summary>
+            /// Gets the short name of the current command line
+            /// <see cref="Option" />.
+            /// </summary>
+            /// <returns>
+            /// The short name of the current <see cref="Option" />.
+            /// </returns>
+            public string ShortName { get; }
+
+            /// <summary>
+            /// Gets the long name of the current command line
+            /// <see cref="Option" />.
+            /// </summary>
+            /// <returns>
+            /// The long name of the current <see cref="Option" />.
+            /// </returns>
+            public string LongName { get; }
+
+            /// <summary>
+            /// Gets the <see cref="Argument" /> occurance of the current
+            /// command line <see cref="Option" />.
+            /// </summary>
+            /// <returns>
+            /// The <see cref="Argument" /> occurance of the current command
+            /// line <see cref="Option" />.
+            /// </returns>
+            public Argument Arguments { get; }
+
+            /// <summary>
+            /// Gets the occurance option of the current command line
+            /// <see cref="Option" />.
+            /// </summary>
+            /// <returns>
+            /// The occurance option of the current command line
+            /// <see cref="Option" />.
+            /// </returns>
+            public Occur Occurs { get; }
+
+            /// <summary>
+            /// Initializes a new <see cref="Option" />.
+            /// </summary>
+            /// <param name="shortName">
+            /// The short name of the command line option.
+            /// </param>
+            /// <param name="longName">
+            /// The long name of the command line option.
+            /// </param>
+            /// <param name="arguments">
+            /// The occurence of a argument after the command line option.
+            /// </param>
+            /// <param name="occurs">
+            /// The occurance of the command line option.
+            /// </param>
+            /// <exception cref="ArgumentNullException">
+            /// <paramref name="shortName"/> or <paramref name="longName"/>
+            /// is <c>null</c>.
+            /// </exception>
+            public Option(
+                string shortName,
+                string longName,
+                Argument arguments,
+                Occur occurs ) {
+
+                ShortName = shortName
+                    ?? throw new ArgumentNullException( nameof( shortName ) );
+                LongName = longName
+                    ?? throw new ArgumentNullException( nameof( longName ) );
+                Arguments = arguments;
+                Occurs = occurs;
+            }
+
+            /// <inheritdoc />
+            public bool Equals( Option other ) {
+                if ( this == other ) {
+                    return true;
+                }
+
+                return other != null
+                    && ShortName == other.ShortName
+                    && LongName == other.LongName
+                    && Arguments == other.Arguments
+                    && Occurs == other.Occurs;
+            }
+
+            /// <inheritdoc />
+            public override bool Equals( object obj )
+                => Equals( obj as Option );
+
+            /// <inheritdoc />
+            public override int GetHashCode() {
+                unchecked {
+                    int hash = 31;
+                    hash = ( hash * 17 ) + ShortName.GetHashCode();
+                    hash = ( hash * 17 ) + LongName.GetHashCode();
+                    hash = ( hash * 17 ) + Arguments.GetHashCode();
+                    hash = ( hash * 17 ) + LongName.GetHashCode();
+                    return hash;
+                }
+            }
+
+            /// <inheritdoc />
+            public override string ToString()
+                => $"{ShortName}, {LongName}, {Arguments}, {Occurs}";
+        }
     }
 
     /*
